9d0dbb12d7f4531802ecd938f25b2c5a45377767
Jael Marta <jachiis77@gmail.com>
2022-10-20 00:39:37 -0300

    comienzo de proyecto

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Nav from "./Nav"
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="redes">
+                <a href="https://www.facebook.com/">facebook</a>
+                <a href="https://www.instagram.com/">instagram</a>
+                <a href="https://www.twitter.com/">twitter</a>
+            </div>
+            <div>
+                <Nav/>
+            </div>
+        </footer>
+    )
+}
+
+export default footer
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const NavBar = () => {
+    return (
+        <nav>
+            <a href="#">link 1</a>
+            <a href="#">link 2</a>
+            <a href="#">link 3</a>
+        </nav>
+    )
+}
+
+export default NavBar
